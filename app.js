cd8f9911da6885f4dd8831df42adc1ffc3282119
Matias Bassi <mebassi87@gmail.com>
2024-02-18 00:39:36 -0300

    proyecto final

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 function encriptar() {
-    var texto = document.querySelector(".ingreso-texto").value;
-    var textoCifrado = texto
-      .replace(/a/gi, "ai")
-      .replace(/e/gi, "enter")
-      .replace(/i/gi, "imes")
-      .replace(/o/gi, "ober")
-      .replace(/u/gi, "ufat");
-    document.querySelector(".texto-encriptado").value = textoCifrado;
-  }
+  var texto = document.querySelector(".ingreso-texto").value;
+  var textoCifrado = texto
+  .replace(/e/gi, "enter")
+  .replace(/i/gi, "imes")  
+  .replace(/a/gi, "ai")
+  .replace(/o/gi, "ober")
+  .replace(/u/gi, "ufat");
+  document.querySelector(".texto-encriptado").value = textoCifrado;
+}
+
   
-  function desencriptar() {
-    var texto = document.querySelector(".texto-encriptado").value;
-    var textoDesencriptado = texto
-      .replace(/ai/gi, "a")
-      .replace(/enter/gi, "e")
-      .replace(/imes/gi, "i")
-      .replace(/ober/gi, "o")
-      .replace(/ufat/gi, "u");
-    document.querySelector(".texto-encriptado").value = textoDesencriptado;
-  }
-  
-  document.querySelector("#btn-encriptar").addEventListener("click", encriptar);
-  document.querySelector("#btn-desencriptar").addEventListener("click", desencriptar);
+function desencriptar() {
+  var texto = document.querySelector(".ingreso-texto").value;
+  var textoDesencriptado = texto
+    .replace(/ai/gi, "a")
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  document.querySelector(".texto-encriptado").value = textoDesencriptado;
+}
+
+function copiar(){
+  var textoEncriptado = document.querySelector(".texto-encriptado").value;
+  document.querySelector(".ingreso-texto").value = textoEncriptado;
+}
+
+document.querySelector("#btn-encriptar").addEventListener("click", encriptar);
+document.querySelector("#btn-desencriptar").addEventListener("click", desencriptar);
+document.querySelector("#btn-copy").addEventListener("click", copiar);
+
